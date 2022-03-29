5c5ad3c9e5f7b5bf10a953f0de9049a375e98d0c
Janne Lempinen <janne.lempinen@helsinki.fi>
2022-03-29 18:02:52 +0300

    Unicafe testikattavuus 100 %

--- laskarit/viikko2/unicafe/src/tests/kassapaate_test.py
+++ laskarit/viikko2/unicafe/src/tests/kassapaate_test.py
@@ -4,7 +4,9 @@ from kassapaate import Kassapaate
 
 class TestKassapaate(unittest.TestCase):
 	def setUp(self):
-		paate = Kassapaate()
+		self.paate = Kassapaate()
+		self.rahakas_kortti = Maksukortti(1000)
+		self.koyhan_kortti = Maksukortti(100)
     
 	def test_luodun_kassapaatteen_saldo_oikein(self):
 		self.assertEqual(self.paate.kassassa_rahaa, 100000)
@@ -15,16 +17,111 @@ class TestKassapaate(unittest.TestCase):
 	def test_luodun_kassapaatteen_edulliset_oikein(self):
 		self.assertEqual(self.paate.maukkaat, 0)
 
-	def test_edullinen_kateisella_kasvattaa_saldoa(self):
+	def test_edullinen_kateisella_kasvattaa_kassaa(self):
 		self.paate.syo_edullisesti_kateisella(500)
-		self.assertEqual(self.paate.kassassa_rahaa(100240)
+		self.assertEqual(self.paate.kassassa_rahaa, 100240)
 
 	def test_edullinen_kateisella_vaihtoraha_oikein(self):
- 		assertEqual(self.paate.syo_edullisesti_kateisella(500), 260)
+ 		self.assertEqual(self.paate.syo_edullisesti_kateisella(500), 260)
 
-	def test_maukkaasti_kateisella_kasvattaa_saldoa(self):
+	def test_maukkaasti_kateisella_kasvattaa_kassaa(self):
 		self.paate.syo_maukkaasti_kateisella(500)
-		self.assertEqual(self.paate.kassassa_rahaa, 100500)
+		self.assertEqual(self.paate.kassassa_rahaa, 100400)
 	
 	def test_maukkaasti_kateisella_vaihtoraha_oikein(self):
-		assertEqual(self.paate.syo_maukkaasti_kateisella(500), 100)
+		self.assertEqual(self.paate.syo_maukkaasti_kateisella(500), 100)
+	
+	def test_edullisesti_kateisella_palauttaa_kaiken(self):
+		self.assertEqual(self.paate.syo_edullisesti_kateisella(200), 200)
+
+	def test_maukkaasti_kateisella_palauttaa_kaiken(self):
+		self.assertEqual(self.paate.syo_maukkaasti_kateisella(200), 200)
+
+	def test_edullisesti_kateisella_ei_muuta_saldoa(self):
+		self.paate.syo_edullisesti_kateisella(200)
+		self.assertEqual(self.paate.kassassa_rahaa, 100000)
+
+	def test_maukkaasti_kateisella_ei_muuta_saldoa(self):
+		self.paate.syo_maukkaasti_kateisella(200)
+		self.assertEqual(self.paate.kassassa_rahaa, 100000)
+
+	def test_edullisesti_kateisella_ei_muuta_maaraa(self):
+		self.paate.syo_edullisesti_kateisella(200)
+		self.assertEqual(self.paate.edulliset, 0)
+
+	def test_maukkaasti_kateisella_ei_muuta_maaraa(self):
+			self.paate.syo_maukkaasti_kateisella(200)
+			self.assertEqual(self.paate.maukkaat, 0)
+
+	def test_syo_edullisesti_kortilla_palauttaa_True(self):
+		self.assertEqual(self.paate.syo_edullisesti_kortilla(self.rahakas_kortti), True)
+
+	def test_syo_edullisesti_kortilla_vahentaa_kortin_saldoa(self):
+		self.paate.syo_edullisesti_kortilla(self.rahakas_kortti)
+		self.assertEqual(str(self.rahakas_kortti), "saldo: 7.6")
+	
+	def test_syo_edullisesti_kortilla_kasvattaa_maaraa(self):
+		self.paate.syo_edullisesti_kortilla(self.rahakas_kortti)
+		self.assertEqual(self.paate.edulliset, 1)
+	
+	def test_syo_edullisesti_kortilla_ei_kasvata_kassaa(self):
+		self.paate.syo_edullisesti_kortilla(self.rahakas_kortti)
+		self.assertEqual(self.paate.kassassa_rahaa, 100000)
+	
+	def test_syo_edullisesti_kortilla_palauttaa_false_jos_saldo_liian_pieni(self):
+		self.assertEqual(self.paate.syo_edullisesti_kortilla(self.koyhan_kortti), False)
+	
+	def test_syo_edullisesti_kortilla_ei_kasvata_kassaa_jos_saldo_liian_pieni(self):
+		self.paate.syo_edullisesti_kortilla(self.koyhan_kortti)
+		self.assertEqual(self.paate.kassassa_rahaa, 100000)
+
+	def test_syo_edullisesti_kortilla_ei_kasvata_maaraa_jos_saldo_liian_pieni(self):
+		self.paate.syo_edullisesti_kortilla(self.koyhan_kortti)
+		self.assertEqual(self.paate.edulliset, 0)
+	
+	def test_syo_maukkaasti_kortilla_vahentaa_kortin_saldoa(self):
+		self.paate.syo_maukkaasti_kortilla(self.rahakas_kortti)
+		self.assertEqual(str(self.rahakas_kortti), "saldo: 6.0")
+	
+	def test_syo_maukkaasti_kortilla_kasvattaa_maaraa(self):
+		self.paate.syo_maukkaasti_kortilla(self.rahakas_kortti)
+		self.assertEqual(self.paate.maukkaat, 1)
+	
+	def test_syo_maukkaasti_kortilla_ei_kasvata_kassaa(self):
+		self.paate.syo_maukkaasti_kortilla(self.rahakas_kortti)
+		self.assertEqual(self.paate.kassassa_rahaa, 100000)
+	
+	def test_syo_maukkaasti_kortilla_palauttaa_false_jos_saldo_liian_pieni(self):
+		self.assertEqual(self.paate.syo_maukkaasti_kortilla(self.koyhan_kortti), False)
+	
+	def test_syo_maukkaasti_kortilla_ei_kasvata_kassaa_jos_saldo_liian_pieni(self):
+		self.paate.syo_maukkaasti_kortilla(self.koyhan_kortti)
+		self.assertEqual(self.paate.kassassa_rahaa, 100000)
+
+	def test_syo_maukkaasti_kortilla_ei_kasvata_maaraa_jos_saldo_liian_pieni(self):
+		self.paate.syo_maukkaasti_kortilla(self.koyhan_kortti)
+		self.assertEqual(self.paate.maukkaat, 0)
+	
+	def test_syo_edullisesti_kortilla_ei_muuta_saldoa_jos_saldo_liian_pieni(self):
+		self.paate.syo_edullisesti_kortilla(self.koyhan_kortti)
+		self.assertEqual(str(self.koyhan_kortti), "saldo: 1.0")
+	
+	def test_syo_maukkaasti_kortilla_ei_muuta_saldoa_jos_saldo_liian_pieni(self):
+		self.paate.syo_maukkaasti_kortilla(self.koyhan_kortti)
+		self.assertEqual(str(self.koyhan_kortti), "saldo: 1.0")
+
+	def test_rahan_lataaminen_kasvattaa_kortin_saldoa(self):
+		self.paate.lataa_rahaa_kortille(self.koyhan_kortti, 300)
+		self.assertEqual(str(self.koyhan_kortti), "saldo: 4.0")
+
+	def test_rahan_lataaminen_kasvattaa_kassaa(self):
+		self.paate.lataa_rahaa_kortille(self.koyhan_kortti, 300)
+		self.assertEqual(self.paate.kassassa_rahaa, 100300)
+	
+	def test_negatiivinen_lataus_ei_muuta_kortin_saldoa(self):
+		self.paate.lataa_rahaa_kortille(self.koyhan_kortti, -100)
+		self.assertEqual(str(self.koyhan_kortti), "saldo: 1.0")
+	
+	def test_negatiivinen_lataus_ei_kasvata_kassaa(self):
+		self.paate.lataa_rahaa_kortille(self.koyhan_kortti, -100)
+		self.assertEqual(self.paate.kassassa_rahaa, 100000)
