0e9811d17b80f2d6852b886ec215b1e35d4d3c17
Janne Lempinen <janne.lempinen@helsinki.fi>
2022-04-02 18:12:59 +0300

    Riittää tältä päivältä!

--- src/index.py
+++ src/index.py
@@ -12,3 +12,13 @@ if __name__ == "__main__":
     play(testgame,2,0)
     print(testgame)
     print(check_winner(testgame))
+
+    testgame2 = TicTacToe()
+    play(testgame2,1,1)
+    play(testgame2,0,0)
+    play(testgame2,1,0)
+    play(testgame2,0,1)
+    play(testgame2,2,0)
+    play(testgame2,0,2)
+    print(testgame2)
+    print(check_winner(testgame2))
