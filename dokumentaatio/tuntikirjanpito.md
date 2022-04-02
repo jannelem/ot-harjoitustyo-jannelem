14d7902d67a3edda781183f89347cc409ecf6fd2
Janne Lempinen <janne.lempinen@helsinki.fi>
2022-04-02 17:55:14 +0300

    Yritystä importtien toimintaan

--- dokumentaatio/tuntikirjanpito.md
+++ dokumentaatio/tuntikirjanpito.md
@@ -4,4 +4,5 @@
 | :----:|:-----|:-----|
 | 28.3. | 2 | aiheen valinnan pohtiminen |
 | 29.3. | 2 | aiheen valinta ja alustava vaatimusmäärittely |
+| 2.4. | 5 | epätoivoisia yrityksiä saada importit toimimaan, ohjelmointia ei pääse kunnolla aloittamaan |
 | yht. | 4 ||
--- src/services/game_service.py
+++ src/services/game_service.py
@@ -1,11 +1,31 @@
 
 from entities.game import TicTacToe
 
-def play(game, row, column):
+def play(game: TicTacToe, row, column):
         if game.board[row][column] == 0:
             game.board[row][column] = game.turn
             game.turn = -game.turn
 
-if __name__ == "__main__":
-    testgame = TicTacToe()
-    print(testgame)
+def check_winner(game):
+    board_size = len(game.board)
+    diagonal1_sum = 0
+    diagonal2_sum = 0
+    for i in range(board_size):
+        diagonal1_sum += game.board[i][i]
+        diagonal2_sum += game.board[i][-1-i]
+        row_sum = 0
+        column_sum = 0
+        for j in range(board_size):
+            row_sum += game.board[i][j]
+            column_sum += game.board[j][i]
+        if abs(row_sum) == board_size:
+            return row_sum
+        if abs(column_sum) == board_size:
+            return column_sum
+    if abs(diagonal1_sum) == board_size:
+        return diagonal1_sum
+    if abs(diagonal2_sum) == board_size:
+        return diagonal2_sum
+    return 0
+
+
